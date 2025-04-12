f5d58798ca5627e670c6982c1eb9a7e1a2211970
chris Taylor <ctay456a@gmail.com>
2025-04-12 01:29:24 -0400

    Create hotel.java

---
+++ hotel.java
@@ -0,0 +1,34 @@
+package com.example.travelgig.model;
+
+public class Hotel {
+
+    private String name;
+    private String address;
+    private String city;
+
+    // --- Getters and Setters ---
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
