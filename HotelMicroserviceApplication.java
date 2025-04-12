47abf64c908b660b6a109dff44cea5fffb4e045c
chris Taylor <ctay456a@gmail.com>
2025-04-12 01:24:17 -0400

    Create HotelMicroserviceApplication.java

---
+++ HotelMicroserviceApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class HotelMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMicroserviceApplication.class, args);
+    }
+}
